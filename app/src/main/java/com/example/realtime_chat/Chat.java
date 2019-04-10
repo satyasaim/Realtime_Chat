fa9c050327923f04330b9b4b41b5c5ec1f3f0786
satyasaim <satyasaimedia3@gmail.com>
2019-04-10 09:36:58 +0530

    My first Android project

---
+++ app/src/main/java/com/example/realtime_chat/Chat.java
@@ -0,0 +1,13 @@
+package com.example.realtime_chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
